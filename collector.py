e0fd2d7fea3220d565af5e874339d2d64e0357b6
David Mbele <dmbele17@gmail.com>
2026-08-31 17:41:39 -0500

    update collecter.py to keep running and update json with data

--- collector.py
+++ collector.py
@@ -1,27 +1,35 @@
 import json
 from datetime import datetime, timezone
+import time
+
+
+# Helper method to write json for temperature 
+def write_json(data, filename="telemetry_data.json"):
+    with open(filename, "a") as f:
+        json.dump(data, f)
+        f.write("\n")  
 
 def main():
+    while True:
+        ##code to get cpu temperature for raspbery Pi 5
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as temprature_file:
+            raw_data = temprature_file.read()
+        
+        temp_celsius = int(raw_data.strip()) / 1000.0
+        time_stamp = datetime.now(timezone.utc)
+        
+        temperature_dict = {
+            "timestamp": time_stamp.isoformat(),
+            "metric": "cpu_temperature",
+            "value": temp_celsius,
+            "unit": "celsius"
+        }
 
-     ##code to get cpu temprature for raspbery Pi 5
-    with open("/sys/class/thermal/thermal_zone0/temp", "r") as temprature_file:
-        raw_data = temprature_file.read()
-
-    temp_celsius = int(raw_data.strip()) / 1000.0
-    time_stamp = datetime.now(timezone.utc)
-
-    temperature_dict = {
-        "timestamp": time_stamp.isoformat(),
-        "metric": "cpu_temperature",
-        "value": temp_celsius,
-        "unit": "celsius"
-    }
-
-    j = json.dumps(temperature_dict)
-    with open("telemetry_data.json", "w") as f:
-        f.write(j)
+        write_json(temperature_dict) 
 
 
+        time.sleep(30)
+        
     ##print(f"CPU Temperature: {temp_celsius} °C")
 
 
